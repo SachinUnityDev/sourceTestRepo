fa5f6e054515eb65158d50627c0bc3519c6512c9
SachinUnityDev <contact.ssingh@gmail.com>
2022-02-21 09:30:21 +0530

    TreeCommit One

---
+++ Assets/MyClass.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyClass : MonoBehaviour
+{
+
+    string str = "Hello World"; 
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
